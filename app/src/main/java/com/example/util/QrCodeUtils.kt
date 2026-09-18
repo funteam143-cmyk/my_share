8c589b6dd46bc326d32d22937a8f915bcd7d0e8b
funteam143-cmyk <funteam143@gmail.com>
2026-09-18 22:37:35 +0530

    Add QR code generator

---
+++ app/src/main/java/com/example/util/QrCodeUtils.kt
@@ -0,0 +1,32 @@
+package com.example.util
+
+import android.graphics.Bitmap
+import android.graphics.Color
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.common.BitMatrix
+import java.util.EnumMap
+
+object QrCodeUtils {
+    fun createQrBitmap(content: String, size: Int = 720): Bitmap {
+        val hints = EnumMap<EncodeHintType, Any>(EncodeHintType::class.java).apply {
+            put(EncodeHintType.MARGIN, 1)
+            put(EncodeHintType.CHARACTER_SET, "UTF-8")
+        }
+        val matrix: BitMatrix = MultiFormatWriter().encode(
+            content,
+            BarcodeFormat.QR_CODE,
+            size,
+            size,
+            hints
+        )
+        val bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888)
+        for (x in 0 until size) {
+            for (y in 0 until size) {
+                bitmap.setPixel(x, y, if (matrix[x, y]) Color.BLACK else Color.WHITE)
+            }
+        }
+        return bitmap
+    }
+}
