9cb81e88e6563622b91914fff5ae4d93942249a1
funteam143-cmyk <funteam143@gmail.com>
2026-09-20 04:49:57 +0530

    Add Google Nearby Connections dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
   implementation(libs.kotlinx.coroutines.android)
   implementation(libs.kotlinx.coroutines.core)
   implementation("com.journeyapps:zxing-android-embedded:4.3.0")
+  implementation("com.google.android.gms:play-services-nearby:19.3.0")
   testImplementation(libs.androidx.compose.ui.test.junit4)
   testImplementation(libs.androidx.core)
   testImplementation(libs.androidx.junit)
